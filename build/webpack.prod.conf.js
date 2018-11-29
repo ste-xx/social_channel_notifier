2adde59e0805b8ee607656b8ef987d410646c6c2
ste-xx <stefan.breitenstein@gmail.com>
2018-11-29 21:18:20 +0100

    fix prod build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,42 +1,128 @@
-const fs = require('fs');
 const path = require('path');
-const baseConfig = require('./webpack.base.conf');
-const merge = require('webpack-merge');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.conf');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const loadMinified = require('./load-minified');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const resolve = (dir) => path.join(__dirname, '..', dir);
 
-const assetsRoot = resolve('.');
+const assetsRoot = resolve('dist');
 const assetsPublicPath = '/';
 const assetsSubDirectory = 'static';
-const cssExtract = false;
+const cssExtract = true;
+const productionGzip = false;
+const productionGzipExtensions = ['js', 'css'];
+const bundleAnalyzerReport = false;
 
-const baseWebpackConfig = baseConfig.webpackBaseConfig(assetsRoot, assetsPublicPath, assetsSubDirectory, cssExtract);
+const assetsPath = (_path) => path.posix.join(assetsSubDirectory, _path);
 
-// add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name]);
-});
-
-module.exports = merge(baseWebpackConfig, {
-  mode: 'development',
-  devtool: '#cheap-module-eval-source-map',
+let webpackConfig = merge(baseConfig.webpackBaseConfig(assetsRoot, assetsPublicPath, assetsSubDirectory, cssExtract), {
+  resolve: {},
+  mode: 'production',
+  devtool: false,
+  output: {
+    path: assetsRoot,
+    filename: assetsPath('js/[name].[chunkhash:15].js'),
+    chunkFilename: assetsPath('js/[id].[chunkhash:15].js')
+  },
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      // new UglifyJsPlugin({
+      //   cache: true,
+      //   parallel: true,
+      //   uglifyOptions: {
+      //     compress: false,
+      //     ecma: 6,
+      //     mangle: true
+      //   },
+      //   sourceMap: true
+      // })
+    ]
+  },
   plugins: [
+    // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
     }),
-    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
+    // extract css into its own file
+    new MiniCssExtractPlugin({
+      filename: assetsPath('css/[name].[contenthash].css')
+    }),
+    // Compress extracted CSS. We are using this plugin so that possible
+    // duplicated CSS from different components can be deduped.
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: {
+        safe: true
+      }
+    }),
+    // generate dist index.html with correct asset hash for caching.
+    // you can customize output by editing /index.html
+    // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
       inject: true,
-      serviceWorkerLoader: `<script>${fs.readFileSync(path.join(__dirname,
-        './service-worker-dev.js'), 'utf-8')}</script>`
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency',
+      serviceWorkerLoader: `<script>${loadMinified(path.join(__dirname,
+        './service-worker-prod.js'))}</script>`
     }),
-    new FriendlyErrorsPlugin()
+    // copy custom static assets
+    new CopyWebpackPlugin([
+      {
+        from: resolve('static'),
+        to: assetsSubDirectory,
+        ignore: ['.*']
+      }
+    ]),
+    // service worker caching
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'my-vue-app',
+      filename: 'service-worker.js',
+      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      minify: true,
+      stripPrefix: 'dist/'
+    })
   ]
 });
+
+if (productionGzip) {
+  const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
+  webpackConfig.plugins.push(
+    new CompressionWebpackPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: new RegExp(
+        '\\.(' +
+        productionGzipExtensions.join('|') +
+        ')$'
+      ),
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  );
+}
+
+if (bundleAnalyzerReport) {
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+  webpackConfig.plugins.push(new BundleAnalyzerPlugin());
+}
+
+module.exports = webpackConfig;
